f914a48f97988ac3f05f8504144fd001ed98b8dd
hkama <doctorteawater@gmail.com>
2018-07-01 22:41:22 +0900

    add matplotlib

--- 3/1.py
+++ 3/1.py
@@ -17,5 +17,33 @@ y = np.argmax(x, axis = 1)
 print(y)
 
 
+train_size = x_train.shape[0]
+batch_size = 10
+batck_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+y_batch = t_train[batch_mask]
+
+
+a = np.array([1,2,3])
+a.reshape(1, a.size)
+
+b = np.array([[1,2], [3,4]])
+b.size
+
+
+import numpy as np
+import matplotlib.pylab as plt
+
+def function_1 (x):
+    return 0.01 * x**2 + 0.1 * x
+
+x = np.arange(0, 20, 1)
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x, y)
+plt.show()
+
+
 
 
